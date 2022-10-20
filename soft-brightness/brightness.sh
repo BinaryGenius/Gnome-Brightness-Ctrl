dc9a3d8bf39c630c6c96cc79dee8e86dbefe95a5
BinaryGenius <105467963+BinaryGenius@users.noreply.github.com>
2022-10-20 03:39:58 -0400

    Create brightness.sh

---
+++ soft-brightness/brightness.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+brightnesslevelpath=/home/$USER/Projects/Soft\ Brightness/brightness-level.txt
+brightness=$(cat "${brightnesslevelpath}")
+
+case $1 in
+	Up)
+		echo $(echo $brightness + 0.05 | bc) > "$brightnesslevelpath"
+		brightness=$(cat "${brightnesslevelpath}")
+		if (( $(bc <<< "$brightness > 1") ))
+		then
+			echo 1 > "$brightnesslevelpath"
+			brightness=$(cat "${brightnesslevelpath}")
+		fi
+		dconf write /org/gnome/shell/extensions/soft-brightness/current-brightness $brightness
+		;;
+	Down)
+		echo $(echo $brightness - 0.05 | bc) > "$brightnesslevelpath"
+		brightness=$(cat "${brightnesslevelpath}")
+		if (( $(bc <<< "$brightness < .1") ))
+		then
+			echo .1 > "$brightnesslevelpath"
+			brightness=$(cat "${brightnesslevelpath}")
+		fi
+		dconf write /org/gnome/shell/extensions/soft-brightness/current-brightness $brightness
+		;;
+esac
