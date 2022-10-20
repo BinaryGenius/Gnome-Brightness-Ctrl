874ee3cc57acafedc3b5191e43cdd28e05de9672
BinaryGenius <105467963+BinaryGenius@users.noreply.github.com>
2022-10-20 03:50:35 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,5 +7,18 @@ mv gnome-gamma-tool.py /home/$USER/Projects/Gnome-Brightness-Ctrl
 cd ..
 rm -rf gnome-gamma-tool
 cd /home/$USER/Projects/Gnome-Brightness-Ctrl
-mv gnome-gamma-tool/brightness.sh .
-rm -rf gnome-gamma-tool
+
+echo
+	read -p "Type "wayland" or "xorg" to pick desired script: " cmd
+	case $cmd in
+		wayland)
+            mv soft-brightness/brightness.sh .
+            rm -rf soft-brightness
+			;;
+		xorg)
+            mv gnome-gamma-tool/brightness.sh .
+            rm -rf gnome-gamma-tool
+			;;
+  esac
+
+
