0dc822bd6673f99af653d95e189f903a03dfdfba
BinaryGenius <105467963+BinaryGenius@users.noreply.github.com>
2022-10-20 03:37:42 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ cd gnome-gamma-tool
 mv gnome-gamma-tool.py /home/$USER/Projects/Gnome-Brightness-Ctrl
 cd ..
 rm -rf gnome-gamma-tool
+cd /home/$USER/Projects/Gnome-Brightness-Ctrl
+mv gnome-gamma-tool/brightness.sh .
+rm -rf gnome-gamma-tool
